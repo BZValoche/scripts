7d16464054a2c6d57891f69c761f41f82ef71826
KrIsS <59690977+christophe-freijanes@users.noreply.github.com>
2020-01-23 09:22:17 +0100

    Détails
    Version 2.0
    Rajout du scan regedit et rajout du scan de la sante de votre dossier WinSxS

--- Clean_Repair_Store_WinSxS.bat
+++ Clean_Repair_Store_WinSxS.bat
@@ -1,21 +1,27 @@
 @echo off
+color 2
+echo Scan du regedit
+sfc /scannow
+color 3
+echo Scan de la sante de votre dossier WinSxS
+DISM /Online /Cleanup-image /Scanhealth
 color 4
 echo Analyse du store
-Dism.exe /Online /Cleanup-Image /AnalyzeComponentStore
+DISM /Online /Cleanup-Image /AnalyzeComponentStore
 pause
 color 5
 echo Commencer le nettoyage des anciennes versions des composants remplaces
-Dism.exe /Online /Cleanup-Image /StartComponentCleanup
+DISM /Online /Cleanup-Image /StartComponentCleanup
 pause
 color 6
 echo Le commutateur /ResetBase permet de retirer la base des composants obsoletes
-Dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
+DISM /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 pause
 color 7
 echo La commande provoque la reparation du magasin de packages
-dism.exe /online /Cleanup-Image /RestoreHealth
+DISM /Online /Cleanup-Image /RestoreHealth
 pause
 color 8
 echo Finir par le nettoyage des differents service packs installes sur le systeme.
-Dism.exe /Online /Cleanup-Image /SPSuperseded
+DISM /Online /Cleanup-Image /SPSuperseded
 pause
